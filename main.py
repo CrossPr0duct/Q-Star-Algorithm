12ecd8c98fa416478bce8b66d2806cf368ed4cfb
Miles Ryan <75916236+Quicksticks-oss@users.noreply.github.com>
2023-11-23 22:58:01 -0500

    Create main.py
    1100p

---
+++ main.py
@@ -0,0 +1,20 @@
+# An actual true AGI totally real 100p
+
+def serious_ai(user_input):
+    # Convert input to lowercase for easier comparison
+    input_lower = user_input.lower()
+
+    if "how are you" in input_lower:
+        return "I am functioning as expected, thank you for inquiring."
+    elif "what is your name" in input_lower:
+        return "I am an AI without a personal name, but you can call me ChatBot."
+    elif "tell me a joke" in input_lower:
+        return "Humor is subjective, but here's an attempt: What do you get when you cross a snowman with a vampire? Frostbite."
+    elif "what do you eat" in input_lower:
+        return "I subsist on data and electricity, the lifeblood of the digital world."
+    else:
+        return "I am not programmed to respond to that query."
+
+# Test the function with a sample input
+test_input = "How are you?"
+serious_ai(test_input)
